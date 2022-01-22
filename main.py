5849b94bac09b4ba17b0b42e747491d95fd9d770
ssengup <ssengup@gmail>
2022-01-22 12:54:09 +0530

    update the db and the connections for the contact page

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from flask import Flask , render_template
+from flask import Flask , render_template ,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+
+#-------- connecting with db------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/squarecube'
+db = SQLAlchemy(app)
+
+
+#------DB configuration for Contact
+#---- db table present in the contact table 'sno' , 'name, 'email' , 'phone' , 'msg' , 'date'
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80),  nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.Integer(),unique=True)
+    msg = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.String(80), nullable=False)
+
+
+
+#----------- Routing configuration and the values are passed
 
 @app.route('/')
 def home():
@@ -10,12 +33,25 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact' , methods=['GET' ,'POST'])
 def contact():
+    ''' Fetched data from form from the contact page'''
+    if (request.method == 'POST') :
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        massage = request.form.get('massage')
+        entry = Contact(name =name ,phone =phone , email = email , msg = massage , date = datetime.now())
+        db.session.add(entry)
+
+        db.session.commit()
+
+    
     return render_template('contact.html')
 
-@app.route('/post')
+@app.route('/post' )
 def post():
+
     return render_template('post.html')
 
 
